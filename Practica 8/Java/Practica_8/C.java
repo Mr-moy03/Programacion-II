b45bd0662fea5a083183444a49968fbb124f6237
Mr-moy03 <153541189+Mr-moy03@users.noreply.github.com>
2025-04-20 21:17:56 -0400

    Update C.java

--- Practica 8/Java/Practica_8/C.java
+++ Practica 8/Java/Practica_8/C.java
@@ -1,39 +1,35 @@
 //C.java
 package Practica_8;
 
-class C {
-    A aParte;
-    B bParte;
+class C extends A implements IncrementaYZ{
+    B b;
 
     public C(int x, int y, int z) {
-        aParte = new A(x, z);
-        bParte = new B(y, z);
+        super(x,z);
+        b = new B(y, z);
     }
 
     public void incrementaXYZ() {
-        aParte.x += 1;
-        bParte.y += 1;
-        aParte.z += 1;
-        bParte.z = aParte.z; // sincronizamos z
+        x += 1;
+        b.y += 1;
+        z += 1;
+        b.z = z;
     }
 
-    public void incrementaZ() {
-        aParte.incrementaZ();
-        bParte.z = aParte.z;
-    }
-
-    public void incrementaXZ() {
-        aParte.incrementaXZ();
-        bParte.z = aParte.z;
-    }
-
+    @Override
     public void incrementaYZ() {
-        bParte.incrementaYZ();
-        aParte.z = bParte.z;
+        b.incrementaYZ();
+        this.z = b.z;
+    }
+
+    @Override
+    public void incrementaZ() {
+        super.incrementaZ();
+        b.z = this.z;
     }
 
     @Override
     public String toString() {
-        return "x:" + aParte.x + ", y:" + bParte.y + ", z:" + aParte.z;
+        return "x:" + this.x + ", y:" + b.y + ", z:" + this.z;
     }
 }
